fa35b686d5aa23293e23217ca13ba8e93e4b3ea9
AnicetFantomas <anicetmurhula20@gmail.com>
2023-11-21 12:53:03 +0200

    Add Welcome and search job bar

--- app/index.js
+++ app/index.js
@@ -1,28 +1,45 @@
-import {View, Text, ScrollView, SafeAreaView} from 'react-native';
-import {Stack, useRouter} from 'expo-router';
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
-import {COLORS, icons, images, SIZES} from '../constants';
-import {Header, HomeHeader, Nearbyjobs, Popularjobs, ScreenHeaderBtn} from '../components';
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Header,
+  HomeHeader,
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
-    const router = useRouter();
-    return (
-        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
-            <Stack.Screen
-            options={{
-                headerStyle:{backgroundColor: COLORS.lightWhite},
-                headerShadowVisible: false,
-                headerLeft: () => {
-                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
-                },
+  const router = useRouter();
 
-                headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={images.profile} dimension="100%"/>
-                ),
-            }}
-            />
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" handlePress={() => {/* handle menu press */}}/>
+          ),
+
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" handlePress={() => {/* handle menu press */}} />
+          ),
+
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
 import styles from './welcome.style'
+import { View, Text,TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { icons, SIZES } from '../../../constants'
+import { useRouter } from 'expo-router'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View style={styles.container}>
+      <Text style={styles.userName}>Hello, Anicet</Text>
+      <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} value='' onChange={() => { }} placeholder="What are you looking for ?" placeholderTextColor="#5b647b" />
+        </View>
+      </View>
+      <TouchableOpacity>
+        
+      </TouchableOpacity>
     </View>
   )
 }
