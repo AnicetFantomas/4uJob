a283d2e7609c79cfb737995ac5b0749d7df2fbef
AnicetFantomas <anicetmurhula20@gmail.com>
2023-11-24 12:26:19 +0200

    add job tabs details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -21,11 +21,14 @@ import {
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
+const tabs = ["About", "Qualifications", "Responsibilities"];
+
 const JobDetails = () => {
   const params = useSearchParams();
   const router = useRouter();
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
  const onRefresh = () => {
 
@@ -68,7 +71,9 @@ const JobDetails = () => {
                         location={data[0].job_country}
                     />
                     <JobTabs
-                    
+                        tabs={tabs}
+                        activeTab={activeTab}
+                        setActiveTab={setActiveTab}
                     />
                 </View>
             )
