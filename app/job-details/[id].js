c6b18bcd77c5b02a252bc1c0fc7bf5fe7196697e
AnicetFantomas <anicetmurhula20@gmail.com>
2023-11-24 12:08:03 +0200

    Company details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -59,11 +59,12 @@ const JobDetails = () => {
            {
             isLoading ? (
                 <ActivityIndicator size='large' color={COLORS.primary} />
-            ) : error ? (<Text>No data available</Text>) : (
-                <View style={{ padding: SIZES.medium, paddingBottom: 0 }}>
+            ) : error ? (<Text>Something went wrong</Text>) : data.length === 0 ? (<Text>No data available</Text>) : (
+                <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
                     <Company
-                        company={data[0].employer_logo}
+                        companyLogo={data[0].employer_logo}
                         jobTitle={data[0].job_title}
+                        companyName={data[0].employer_name}
                         location={data[0].job_country}
                     />
                     <JobTabs
