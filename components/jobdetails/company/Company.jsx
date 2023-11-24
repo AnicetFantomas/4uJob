2762683600899124d2ca136d12e3adc41300b7ef
AnicetFantomas <anicetmurhula20@gmail.com>
2023-11-24 11:19:40 +0200

    Add job details content

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 import styles from './company.style'
+import { icons } from '../../../constants'
+import { checkImageURL } from '../../../utils'
 
 const Company = () => {
   return (
     <View>
-      <Text>Company</Text>
+      
     </View>
   )
 }
